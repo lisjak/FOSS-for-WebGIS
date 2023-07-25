6c549b0a5f12063dd3352b2abb219416eeb3185e
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2023-07-24 20:48:14 -0400

    fix async issue??

--- final-project/script.js
+++ final-project/script.js
@@ -14,7 +14,7 @@ const map = new maplibregl.Map({
 // Add zoom and rotation controls to the map.
 map.addControl(new maplibregl.NavigationControl());
 
-map.once('load', main);
+// map.once('load', main);
 
 
 map.on('load', () => {
@@ -45,137 +45,143 @@ map.on('load', () => {
 
 
 
-// ============ Luxury Housing Points============
-// TO DO: accrue more
-// TO DO: color or size grade point according to rent?
+// // ============ Luxury Housing Points============
+// // TO DO: accrue more
+// // TO DO: color or size grade point according to rent?
+// async function main() {
+
+//     // ============ Cultural Institutions Points============
+//     const culturalInstitutions = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/cultural-institutions.geojson');
+
+//     await axios('./data');
+
+//     map.addSource('cultural-institutions', {
+//         'type': 'geojson',
+//         'data': culturalInstitutions.data
+//     });
+
+
+//     map.addLayer({
+//         'id': 'cultural-points',
+//         'source': 'cultural-institutions',
+//         'type': 'circle',
+//         'paint': {
+//             'circle-color': 'black',
+//             'circle-stroke-width': 2,
+//             'circle-stroke-color': '#fff'
+//         }
+//     })
+//     addEvents();
+//     console.log('culture')
+
+
+//     function addEvents() {
+//         const popup = new maplibregl.Popup({
+//             closeOnClick: false,
+//         });
+
+
+//         map.on('mouseenter', 'cultural-points', (e) => {
+//             console.log('mouse')
+
+//             // Change the cursor style as a UI indicator.
+//             map.getCanvas().style.cursor = 'pointer';
+
+//             const coordinates = e.features[0].geometry.coordinates.slice();
+//             const props = e.features[0].properties;
+
+//             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+//                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+//             }
+//             console.log(props)
+//             popup.setLngLat(coordinates).setHTML(
+//                 `
+//                 <div class='popup-style'>
+//                 <b>${props.NAME}</b> |
+//                 ${props.ADDRESS}<br><br>
+//                 ${props.DESCRIPTION}
+//                 `
+//             ).addTo(map).setMaxWidth("350px");
+//         });
+//     }
+
+
+//     const luxuryHousing = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/luxury-housing.geojson');
+
+//     map.addSource('luxury-housing', {
+//         'type': 'geojson',
+//         'data': luxuryHousing.data
+//     });
+
+//     map.addLayer({
+//         'id': 'housing-points',
+//         'source': 'luxury-housing',
+//         'type': 'circle',
+//         'paint': {
+//             'circle-color': 'red',
+//             'circle-stroke-width': 2,
+//             'circle-stroke-color': '#fff'
+//         }
+//     })
+//     addEvents2();
+//     console.log('housing')
+
+
+//     function addEvents2() {
+//         const popup2 = new maplibregl.Popup({
+//             closeOnClick: false,
+
+//         });
+
+
+//         map.on('mouseenter', 'housing-points', (e) => {
+//             console.log('mouse2')
+
+//             // Change the cursor style as a UI indicator.
+//             map.getCanvas().style.cursor = 'pointer';
+
+//             const coordinates = e.features[0].geometry.coordinates.slice();
+//             const props = e.features[0].properties;
+
+//             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+//                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+//             }
+//             console.log(props)
+//             popup2.setLngLat(coordinates).setHTML(
+//                 `
+//             <div class='popup-style'>
+//             <b><a href="${props.LINK}">${props.NAME}</a></b> |
+//             ${props.ADDRESS}<br>
+//             <b>Highest rent:</b> ${props.RENT} / month
+
+//             <br><sub>Data collection date: ${props.DATE}</sub>
+
+//             `
+//             ).addTo(map);
+//             console.log('popup house')
+//         });
+//     }
+
+
+
+// ============ Greenpoint Polygon ============
+console.log('hi1')
+
+
+map.once('load', main);
+
 async function main() {
-
-    // ============ Cultural Institutions Points============
-    const culturalInstitutions = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/cultural-institutions.geojson');
-
-    await axios('./data');
-
-    map.addSource('cultural-institutions', {
+    let greenpointGeojson = await axios('./data/greenpoint-bounds.geojson');
+    map.addSource('greenpoint-src', {
         'type': 'geojson',
-        'data': culturalInstitutions.data
+        'data': greenpointGeojson.data
     });
 
-
-    map.addLayer({
-        'id': 'cultural-points',
-        'source': 'cultural-institutions',
-        'type': 'circle',
-        'paint': {
-            'circle-color': 'black',
-            'circle-stroke-width': 2,
-            'circle-stroke-color': '#fff'
-        }
-    })
-    addEvents();
-    console.log('culture')
-
-
-    function addEvents() {
-        const popup = new maplibregl.Popup({
-            closeOnClick: false,
-        });
-
-
-        map.on('mouseenter', 'cultural-points', (e) => {
-            console.log('mouse')
-
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
-
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const props = e.features[0].properties;
-
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-            console.log(props)
-            popup.setLngLat(coordinates).setHTML(
-                `
-                <div class='popup-style'>
-                <b>${props.NAME}</b> |
-                ${props.ADDRESS}<br><br>
-                ${props.DESCRIPTION}
-                `
-            ).addTo(map).setMaxWidth("350px");
-        });
-    }
-
-
-    const luxuryHousing = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/luxury-housing.geojson');
-
-    map.addSource('luxury-housing', {
-        'type': 'geojson',
-        'data': luxuryHousing.data
-    });
-
-    map.addLayer({
-        'id': 'housing-points',
-        'source': 'luxury-housing',
-        'type': 'circle',
-        'paint': {
-            'circle-color': 'red',
-            'circle-stroke-width': 2,
-            'circle-stroke-color': '#fff'
-        }
-    })
-    addEvents2();
-    console.log('housing')
-
-
-    function addEvents2() {
-        const popup2 = new maplibregl.Popup({
-            closeOnClick: false,
-
-        });
-
-
-        map.on('mouseenter', 'housing-points', (e) => {
-            console.log('mouse2')
-
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
-
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const props = e.features[0].properties;
-
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-            console.log(props)
-            popup2.setLngLat(coordinates).setHTML(
-                `
-            <div class='popup-style'>
-            <b><a href="${props.LINK}">${props.NAME}</a></b> |
-            ${props.ADDRESS}<br>
-            <b>Highest rent:</b> ${props.RENT} / month
-
-            <br><sub>Data collection date: ${props.DATE}</sub>
-
-            `
-            ).addTo(map);
-            console.log('popup house')
-        });
-    }
-
-
-
-    // ============ Greenpoint Polygon ============
-    const greenpointBounds = await axios('./data/greenpoint-bounds.geojson');
-
-    map.addSource('greenpoint-bounds', {
-        'type': 'geojson',
-        'data': greenpointBounds.data
-    });
 
     map.addLayer({
         'id': 'greenpoint-polygon',
         'type': 'line',
-        'source': 'greenpoint-bounds',
+        'source': 'greenpoint-src',
         'layout': {
             'line-join': 'round',
             'line-cap': 'round'
@@ -184,7 +190,8 @@ async function main() {
             'line-color': 'red',
             'line-width': 8
         }
-    })
-    addEvents();
-    console.log('polygon')
+    });
+
+    // addEvents();
+
 }
