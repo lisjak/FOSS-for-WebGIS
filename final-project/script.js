39a8b4586e9f0f4591f193f515815d3c3947595a
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2023-08-16 05:20:21 -0400

    final proj

--- final-project/script.js
+++ final-project/script.js
@@ -5,19 +5,16 @@ const sliderValue = document.getElementById('slider-value');
 const map = new maplibregl.Map({
     container: 'map',
     style: 'https://api.maptiler.com/maps/streets/style.json?key=Qx8CiAhWm4RayySUrlIv',
-    center: [20.977900, 52.241018],
+    center: [20.9962, 52.24186],
     zoom: 14,
-    pitch: 15,
+    pitch: 45,
     bearing: -50,
     hash: true
 });
 
 
-
 // Add zoom and rotation controls to the map.
 map.addControl(new maplibregl.NavigationControl());
-
-// map.once('load', main);
 
 
 map.on('load', () => {
@@ -44,14 +41,18 @@ map.on('load', () => {
     })
 })
 
+// ============ Control Panel ============
 map.on('load', function() {
     const targets = {
         warsawGhettoArea: "Warsaw Ghetto Area",
-        nyc: "Map 1"
-            // water: "Water",
-            // building: "Building",
-            // airport: "Airport",
-            // poi: "Pois",
+        ghettoMap: "Historical Map",
+        warsawGhettoPointsGeojson: "Ghetto Locations",
+        warsawHousingPointsGeojson: "Contemporary Luxury Housing"
+
+        // water: "Water",
+        // building: "Building",
+        // airport: "Airport",
+        // poi: "Pois",
     };
     const options = {
         title: "Controls",
@@ -64,127 +65,7 @@ map.on('load', function() {
 });
 
 
-
-// // ============ Luxury Housing Points============
-// // TO DO: accrue more
-// // TO DO: color or size grade point according to rent?
-// async function main() {
-
-//     // ============ Cultural Institutions Points============
-//     const culturalInstitutions = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/cultural-institutions.geojson');
-
-//     await axios('./data');
-
-//     map.addSource('cultural-institutions', {
-//         'type': 'geojson',
-//         'data': culturalInstitutions.data
-//     });
-
-
-//     map.addLayer({
-//         'id': 'cultural-points',
-//         'source': 'cultural-institutions',
-//         'type': 'circle',
-//         'paint': {
-//             'circle-color': 'black',
-//             'circle-stroke-width': 2,
-//             'circle-stroke-color': '#fff'
-//         }
-//     })
-//     addEvents();
-//     console.log('culture')
-
-
-//     function addEvents() {
-//         const popup = new maplibregl.Popup({
-//             closeOnClick: false,
-//         });
-
-
-//         map.on('mouseenter', 'cultural-points', (e) => {
-//             console.log('mouse')
-
-//             // Change the cursor style as a UI indicator.
-//             map.getCanvas().style.cursor = 'pointer';
-
-//             const coordinates = e.features[0].geometry.coordinates.slice();
-//             const props = e.features[0].properties;
-
-//             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-//                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-//             }
-//             console.log(props)
-//             popup.setLngLat(coordinates).setHTML(
-//                 `
-//                 <div class='popup-style'>
-//                 <b>${props.NAME}</b> |
-//                 ${props.ADDRESS}<br><br>
-//                 ${props.DESCRIPTION}
-//                 `
-//             ).addTo(map).setMaxWidth("350px");
-//         });
-//     }
-
-
-//     const luxuryHousing = await axios('https://corsproxy.org/?https://raw.githubusercontent.com/lisjak/FOSS-for-WebGIS/main/homework-3/data/luxury-housing.geojson');
-
-//     map.addSource('luxury-housing', {
-//         'type': 'geojson',
-//         'data': luxuryHousing.data
-//     });
-
-//     map.addLayer({
-//         'id': 'housing-points',
-//         'source': 'luxury-housing',
-//         'type': 'circle',
-//         'paint': {
-//             'circle-color': 'red',
-//             'circle-stroke-width': 2,
-//             'circle-stroke-color': '#fff'
-//         }
-//     })
-//     addEvents2();
-//     console.log('housing')
-
-
-//     function addEvents2() {
-//         const popup2 = new maplibregl.Popup({
-//             closeOnClick: false,
-
-//         });
-
-
-//         map.on('mouseenter', 'housing-points', (e) => {
-//             console.log('mouse2')
-
-//             // Change the cursor style as a UI indicator.
-//             map.getCanvas().style.cursor = 'pointer';
-
-//             const coordinates = e.features[0].geometry.coordinates.slice();
-//             const props = e.features[0].properties;
-
-//             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-//                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-//             }
-//             console.log(props)
-//             popup2.setLngLat(coordinates).setHTML(
-//                 `
-//             <div class='popup-style'>
-//             <b><a href="${props.LINK}">${props.NAME}</a></b> |
-//             ${props.ADDRESS}<br>
-//             <b>Highest rent:</b> ${props.RENT} / month
-
-//             <br><sub>Data collection date: ${props.DATE}</sub>
-
-//             `
-//             ).addTo(map);
-//             console.log('popup house')
-//         });
-//     }
-
-
-
-// ============ Greenpoint Polygon ============
+// ============ Warsaw Ghetto Line ============
 console.log('hi1')
 
 
@@ -211,72 +92,212 @@ async function main() {
             'line-width': 8
         }
     });
-}
+
+    console.log('warsawGhettoArea hit');
+
+    // }
+
+    // ============ Warsaw Ghetto Points ============
+
+    let warsawGhettoPointsGeojson = await axios('./data/warsaw-ghetto-points.geojson');
+    map.addSource('wg-points-src', {
+        'type': 'geojson',
+        'data': warsawGhettoPointsGeojson.data
+    });
+
+    //console log to see the features names
+    console.log('data', warsawGhettoPointsGeojson);
+
+    map.addLayer({
+        'id': 'warsawGhettoPointsGeojson',
+        'type': 'circle',
+        'source': 'wg-points-src',
+        'layout': {},
+        'paint': {
+            'circle-color': 'red',
+            'circle-stroke-width': 2,
+            'circle-stroke-color': 'black'
+        }
+    });
+
+    addEvents();
+
+    function addEvents() {
+
+        const popup = new maplibregl.Popup({
+            closeButton: true,
+            closeOnClick: false
+        });
+
+        map.on('mouseenter', 'warsawGhettoPointsGeojson', (e) => {
+
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const name = e.features[0].properties.NAME;
+            const address = e.features[0].properties.ADDRESS;
+            const description = e.features[0].properties.DESCRIPTION;
+            const imageThen = e.features[0].properties.IMAGE_THEN;
+            const sourceThen = e.features[0].properties.SOURCE_THEN;
+            const imageNow = e.features[0].properties.IMAGE_NOW;
+            const sourceNow = e.features[0].properties.SOURCE_NOW;
+
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            //this is what will display in the popup
+            //can wrap setHTML in div for further styling
+            popup.setLngLat(coordinates).setHTML(
+                `
+            <div class="popup-style">
+            <h3>${name}</h3><br>
+            <img src=" ${imageThen}" width="330px"><br>
+            <b>${address}</b><br>
+            ${description}<br> <a href="${imageThen}" target="_blank" rel="noopener noreferrer">Full-size image</a> |
+            <a href="${sourceThen}">Source</a><br>
+            </div>
+            `
+            ).addTo(map).setMaxWidth("350px");
+        });
+
+        console.log('warsawGhettoPoints hit');
+    }
 
 
+    // ============ Housing Points ============
 
-map.once('load', () => {
-    map.addSource('historical-nyc-src', {
-        "type": "image",
-        "url": "../7-19/site/historical-maps-of-manhattan.jpg",
-        "coordinates": [
-            [
-                20.968276171051343,
-                52.25631970320245
+    let warsawHousingPointsGeojson = await axios('./data/warsaw-luxury-housing.geojson');
+    map.addSource('housing-src', {
+        'type': 'geojson',
+        'data': warsawHousingPointsGeojson.data
+    });
+
+    //console log to see the features names
+    console.log('data', warsawHousingPointsGeojson);
+
+    map.addLayer({
+        'id': 'warsawHousingPointsGeojson',
+        'type': 'circle',
+        'source': 'housing-src',
+        'layout': {},
+        'paint': {
+            'circle-color': 'white',
+            'circle-stroke-width': 2,
+            'circle-stroke-color': 'red'
+        }
+    });
+
+    addEvents2();
+
+    function addEvents2() {
+
+        const popup = new maplibregl.Popup({
+            closeButton: true,
+            closeOnClick: false
+        });
+
+        map.on('mouseenter', 'warsawHousingPointsGeojson', (e) => {
+
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const name = e.features[0].properties.NAME;
+            const rent = e.features[0].properties.RENT;
+            const formatRent = rent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+            function numberWithCommas(x) {}
+            const link = e.features[0].properties.LINK;
+            const aboveMinWage = (rent / 3490).toFixed(2);
+
+
+            // const rentAbove = e.features[0].properties.RENT_ABOVE;
+            // const address = e.features[0].properties.ADDRESS;
+            // const description = e.features[0].properties.DESCRIPTION;
+            // const imageThen = e.features[0].properties.IMAGE_THEN;
+            // const sourceThen = e.features[0].properties.SOURCE_THEN;
+            // const imageNow = e.features[0].properties.IMAGE_NOW;
+            // const sourceNow = e.features[0].properties.SOURCE_NOW;
+
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            //this is what will display in the popup
+            //can wrap setHTML in div for further styling
+            popup.setLngLat(coordinates).setHTML(
+                `
+            <div class="popup-style">
+            <b><a href="${link}">${name}</b></a> <br>
+            ${formatRent} zł./month <br>
+            <b>${aboveMinWage} times above monthly minimum wage</b>
+            </div>
+            `
+            ).addTo(map).setMaxWidth("250px");
+        });
+
+        console.log('warsawHousingPoints hit');
+    }
+
+
+    // ============ Historical Map ============
+
+    let historicalMap = await axios('./data/map.jpg');
+
+    map.addSource('historical-map', {
+        'type': 'image',
+        "url": "./data/map.jpg",
+        'coordinates': [
+
+            [20.96392931529593,
+                52.252603548285435
             ],
-            [
-                21.007894660289338,
-                52.25347267685359
+            [21.011214368336283,
+                52.260769322394026
             ],
-            [
-                20.998765182334296,
-                52.21866092056342
+            [21.02097532160684,
+                52.240510113229305
             ],
-            [
-                20.9600079646018,
-                52.22203780149027
+            [20.971634180701642,
+                52.229758258335835
             ]
+
         ]
     });
 
+
     map.addLayer({
-        'id': 'nyc',
-        'source': 'historical-nyc-src',
-        'type': 'raster'
+        'id': 'ghettoMap',
+        'source': 'historical-map',
+        'type': 'raster',
+        'paint': {
+            'raster-opacity': 0.5,
+        }
     });
 
+    console.log('historical map hit')
+
+    // ============ Layer Slider ============
     slider.addEventListener('input', (e) => {
         // Adjust the layers opacity. layer here is arbitrary - this could
         // be another layer name found in your style or a custom layer
         // added on the fly using `addSource`.
         map.setPaintProperty(
-            'nyc',
+            'ghettoMap',
             'raster-opacity',
             parseInt(e.target.value, 10) / 100
         );
-
-        //     // Value indicator
-        //     sliderValue.textContent = e.target.value + '%';
-        // });
-
-        // let distancePnts = [];
-        // let markers = [];
-
-        // map.on('click', (e) => {
-        //     if (markers.length > 1) {
-        //         markers.forEach(m => m.remove());
-        //         markers = [];
-        //     }
-
-        //     markers.push(new maplibregl.Marker()
-        //         .setLngLat(e.lngLat)
-        //         .addTo(map));
-        //     distancePnts.push(turf.point([e.lngLat.lng, e.lngLat.lat]))
-        //     if (distancePnts.length < 2) return;
-        //     let options = { units: 'miles' };
-        //     let distance = turf.distance(distancePnts[0], distancePnts[1], options);
-        //     console.log(distance);
-        //     distancePnts = [];
     });
 
-});
+
+
+
+}
+
+// }
